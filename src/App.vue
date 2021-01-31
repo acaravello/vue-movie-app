3a32c1ef30cfa4f18ba7b4483e51900feb0dc1f5
Antonio Caravello <acaravello@xeniaprogetti.it>
2021-01-31 12:04:17 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </header>
+
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+
+  @import "~@fontsource/roboto/index.css";
+  @import "~@fontsource/roboto/100.css";
+  @import "~@fontsource/roboto/300.css";
+  @import "~@fontsource/roboto/500.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fefefe;
+  font-family: "Roboto";
+
+  & h1 {
+    font-weight: 100;
+  }
+
+  & h3 {
+    color:   #267953;
+  }
+
+  &::selection {
+    background: transparentize($color: #42b983, $amount: 0.5)
+  }
 }
 
 #nav {
@@ -22,11 +44,37 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #fff;
 
     &.router-link-exact-active {
       color: #42b983;
     }
   }
+
+}
+
+body {
+  background-color: #1c2631 !important;
+
+  a:hover {
+    text-decoration: none ;
+    color: #35ad75 ;
+  }
+  .btn-vue {
+    color: #fff;
+    background-color: #42b983;
+    border-color: #42b983;
+  }
+
+  .btn-vue:hover {
+    color: #fff;
+    background-color: #35ad75;
+    border-color: #35ad75;
+  }
+
+  .btn:focus {
+    box-shadow: none;
+  }
+
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,132 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="movies-list-container">
+      <div class="button-container">
+        <button type="button" class="btn btn-vue check-movies" @click="checkLatestMovies">
+        Check latest movies
+      </button>
+      </div>
+      
+      <div class="cards-container">
+        <div class="card" v-for="movie in latestMovies" :key="movie.id">
+          <img :src="imageRootPath + movie.poster_path" class="card-img-top" :alt="movie.original_title" />
+          <div class="card-body">
+            <h5 class="card-title">{{movie.original_title}}</h5>
+            <p class="card-text">
+              {{movie.overview}}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data() {
+    return {
+      type: "",
+      imageRootPath: "https://image.tmdb.org/t/p/original",
+      latestMovies: [],
+    };
+  },
+  methods: {
+    checkLatestMovies() {
+      console.log("Checking latest movies");
+      const key = process.env.VUE_APP_API_KEY;
+      axios
+        .get(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+        )
+        .then((response) => {
+          console.log("response is");
+          console.log(response);
+          this.latestMovies = response.data.results;
+        })
+        .catch((error) => {
+          console.log("Error in contacting movie db");
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+
+  .movies-list-container {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin: 12px auto 48px auto;
+
+    .button-container {
+      width: 200px;
+      display: flex;
+      justify-content: center;
+      margin:auto;
+    }
+
+    .cards-container {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      width: 100%;
+      justify-content: center;
+
+      .card {
+            display: flex;
+            min-width: 0;
+            background-clip: border-box;
+            border-radius: 0px;
+            flex-direction: column;
+            width: 18rem;
+            margin-left: 24px;
+            margin-right: 24px;
+            margin-bottom: 48px;
+            border:none;
+            box-shadow:  0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+
+            h5 {
+              line-height: 1.7rem;
+              text-transform: uppercase;
+              border-bottom: 1px solid #41B883;
+              padding-bottom: 5px;
+            }
+
+            img {
+              border-radius: 0px;
+            }
+
+            .card-body {
+              background: #35495E;
+              background: -webkit-linear-gradient(to top, #41B883, #35495E);
+              background: linear-gradient(to top, #41B883, #35495E);
+
+              .card-text {
+              font-size: 13px;
+              line-height: 20px;
+              }
+
+            }
+      }
+    }
+
+    .check-movies {
+      margin-bottom: 24px;
+    }
   }
 }
-</script>
+</style>
